1222ef165c5885af0ca25061e5f674148ebc5e64
mildfuzz <john@mildfuzz.com>
2011-11-22 14:29:08 +0000

    Refactored activation method
    Moved core class out of install script.
    
    Added deactivate function

---
+++ core_classes.php
@@ -0,0 +1,66 @@
+<?php
+class PluginTables extends PluginUtilities
+{
+   protected $tables;
+
+   	function __construct(){
+		$args = func_get_args();
+		
+		$this->parse_args($args[0]);
+		
+		
+	}
+	
+	function parse_args($args){
+		foreach($args as $k=>$v){
+			$this->$k = $v;
+		}
+	  }
+	
+	
+	function activate(){
+		global $wpdb;
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		if(!is_array($this->tables)) wp_die('table argument for AdminDatabase must be an array');
+		
+		$sql = array();
+		foreach($this->tables as $table_name => $fields){
+			$table_name = $wpdb->prefix . $table_name;
+			if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
+				//create table
+				$sql[$table_name] =  "CREATE TABLE $table_name (id mediumint(9) NOT NULL AUTO_INCREMENT,";
+					//create fields
+					if(!is_array($fields)) wp_die("$table_name has no fields");
+					foreach ($fields as $k => $sql_command){
+						$sql[$table_name] .= $k." ".$sql_command.",";
+					}
+				$sql[$table_name] .= "UNIQUE KEY id (id));";
+				dbDelta($sql[$table_name]);
+			} 
+		}
+		
+		
+	}
+	
+	function deactivate(){
+		global $wpdb;
+	
+		if(!is_array($this->tables)) wp_die('table argument for AdminDatabase must be an array');
+		foreach($this->tables as $table_name => $fields){
+			$table_name = $wpdb->prefix . $table_name;
+			
+				$wpdb->query("DROP TABLE $table_name;");
+			
+		}
+	}
+}
+
+class PluginUtilities{
+	function parse_args($args){
+		
+		foreach($args as $k=>$v){
+			$this->$k = $v;
+		}
+	  }
+}
+?>
--- plugin.php
+++ plugin.php
@@ -25,7 +25,10 @@ License: GPL2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+//load core classes
+include 'core_classes.php';
 
+global $table;
 $pluginTables = array(
 		'tables' => array(
 					"contacts" => array(
@@ -34,79 +37,26 @@ $pluginTables = array(
 						)
 					)
 				);
-register_activation_hook(__FILE__,'install_plugin');
-//register_deactivation_hook(__FILE__,'app_switcher_uninstall');
+				
+$table = new PluginTables($pluginTables);
+		
+register_activation_hook(__FILE__,'mf_activate_plugin');
+register_deactivation_hook(__FILE__,'mf_deactivate_plugin');
 	
-function install_plugin(){
+function mf_activate_plugin(){
+	global $table;
+	$table->activate();
+}
+function mf_deactivate_plugin(){
+	global $table;
 	
-	//install_tables();
-	
-
-	
+	$table->deactivate();
 }
 
 
-function install_tables(){
-	global $pluginTables;
-	$installTables = new PluginTables($pluginTables);
-	
-}
 
 
-
-class PluginTables extends PluginUtilities
-{
-   protected $tables;
-
-   	function __construct(){
-		$args = func_get_args();
-		$this->parse_args($args[0]);
-		
-		if(array_key_exists('tables',$args[0]))	$this->install_tables(); 
-	}
-	
-	function parse_args($args){
-		foreach($args as $k=>$v){
-			$this->$k = $v;
-		}
-	  }
-	
-	
-	function install_tables(){
-		global $wpdb;
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		fb::log($this->tables,'tables');
-		if(!is_array($this->tables)) wp_die('table argument for AdminDatabase must be an array');
-		
-		$sql = array();
-		foreach($this->tables as $table_name => $fields){
-			$table_name = $wpdb->prefix . $table_name;
-			if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
-				//create table
-				$sql[$table_name] =  "CREATE TABLE $table_name (id mediumint(9) NOT NULL AUTO_INCREMENT,";
-					//create fields
-					if(!is_array($fields)) wp_die("$table_name has no fields");
-					foreach ($fields as $k => $sql_command){
-						$sql[$table_name] .= $k." ".$sql_command.",";
-					}
-				$sql[$table_name] .= "UNIQUE KEY id (id));";
-				dbDelta($sql[$table_name]);
-			} 
-		}
-		
-		
-	}
-}
-
-class PluginUtilities{
-	function parse_args($args){
-		
-		foreach($args as $k=>$v){
-			$this->$k = $v;
-		}
-	  }
-}
-
+//after install
 include 'pages.php';
 
 
