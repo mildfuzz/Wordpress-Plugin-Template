25af421645d59c0d11b102f8de2cfe65875112e9
mildfuzz <john@mildfuzz.com>
2011-11-25 14:12:37 +0000

    aded URL building functions

--- utilities.php
+++ utilities.php
@@ -26,7 +26,10 @@ class mf {
 	 * Just show our message (with possible checking if we only want
 	 * to show message to certain users.
 	 */
-	static function urlQuery($query){
+	function buildUrlQuery($query, $get = true){
+		//merge querys
+		if($get) $query = array_merge($_GET, $query);
+		
 		if(!is_array($query)) die ("urlQuery requires an array as its argument.");
 
 		$url_ext = "?";
@@ -38,6 +41,34 @@ class mf {
 
 		return $url_ext;
 
+	}
+	function removeURLQuery($query){
+		if(is_array($query)){
+			foreach ($query as $v){
+				removeURLQuery($v);
+			}
+		}
+		$get = $_GET;
+		if(array_key_exists($query, $get)) {
+			unset($get[$query]);
+		} 
+		return $get;
+	}
+	function curPageURL() {
+	 	$pageURL = 'http';
+	 	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	 	$pageURL .= "://";
+	 	if ($_SERVER["SERVER_PORT"] != "80") {
+	  		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	 	} else {
+	  		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	 	}
+	 	return $pageURL;
+	}
+	static function urlQuery($query){
+		$curl = explode("?",$_SERVER['REQUEST_URI']);
+		$query = self::buildUrlQuery($query);
+		return $curl[0].$query;
 	}
 }
 
